ba812ea007ec163f1add081a6815518f0acc24c1
Nada Abdelrahman <nadaabdelrahman776@gmail.com>
2021-04-11 01:11:02 +0200

    create method weakly_dominated in Players class , weakly_Player1 method in Player1 and weakly_Player2 method in Player2

--- Player1.py
+++ Player1.py
@@ -45,3 +45,46 @@ class P1():
 
 
 
+    def weakly_Player1(self,weaklyMatrix,weaklyP1_action):
+        n = weaklyMatrix.shape[0]
+        m = weaklyMatrix.shape[1]
+        # player 1
+        arr = np.zeros(n,dtype=int)
+        for i in range(n):
+            arr[i]=i
+        perm = list(itertools.permutations(arr, 2))
+        r = np.random.randint(0,len(perm))
+        c1 = (perm[r])
+        j = 0
+        arr_size = np.size(arr, 0)
+        while (j < m):
+            # print(weaklyMatrix[c1[0]][j][0])
+            # print(weaklyMatrix[c1[1]][j][0])
+            if (weaklyMatrix[c1[0]][j][0] >= weaklyMatrix[c1[1]][j][0]):
+                j += 1
+                if (j == m):
+                    no_delete = set()
+                    weaklyMatrix = np.delete(weaklyMatrix, c1[1], 0)
+                    weaklyP1_action=np.delete(weaklyP1_action,c1[1])
+                    # print(StrictlyMatrix)
+                    no_delete.add(str(c1[1]))
+                    n -= 1
+                    perm = [x for x in perm if not no_delete & set(str(x))]
+                    arr_size -= 1
+
+                    if (len(perm) == 0):
+                        break
+                    else:
+                        r = np.random.randint(0, len(perm))
+                        c1 = perm[r]
+                        j = 0
+            else:
+                perm.remove(perm[r])
+                if (len(perm) == 0):
+                    break
+                else:
+                    r = np.random.randint(0, len(perm))
+                    c1 = perm[r]
+                    j = 0
+        print("p1",weaklyMatrix)
+        return weaklyMatrix,weaklyP1_action
--- Player2.py
+++ Player2.py
@@ -45,3 +45,48 @@ class P2():
                     i= 0
         print("p2", StrictlyMatrix)
         return StrictlyMatrix,StrictP2_action
+
+
+
+    def weakly_Player2(self,weaklyMatrix,weaklyP2_action):
+        #Player2
+        n = weaklyMatrix.shape[0]
+        m = weaklyMatrix.shape[1]
+        arr = np.array([], dtype=int)
+        for i in range(m):
+            arr = np.append(arr, i)
+        perm = list(itertools.permutations(arr, 2))
+        r = np.random.randint(0, len(perm))
+        c1 = (perm[r])
+        i = 0
+        arr_size = np.size(arr, 0)
+        while (i < n):
+            # print(weaklyMatrix[i][c1[0]][1])
+            # print(weaklyMatrix[i][c1[1]][1])
+            if (weaklyMatrix[i][c1[0]][1] >= weaklyMatrix[i][c1[1]][1]):
+                i += 1
+                if (i == n):
+                    no_delete = set()
+                    weaklyMatrix = np.delete(weaklyMatrix, c1[1], 1)
+                    weaklyP2_action=np.delete(weaklyP2_action,c1[1])
+                    no_delete.add(str(c1[1]))
+                    perm = [x for x in perm if not no_delete & set(str(x))]
+                    arr_size -= 1
+                    m-=1
+
+                    if (len(perm) == 0):
+                        break
+                    else:
+                        r = np.random.randint(0, len(perm))
+                        c1 = perm[r]
+                        i = 0
+            else:
+                perm.remove(perm[r])
+                if (len(perm) == 0):
+                    break
+                else:
+                    r = np.random.randint(0, len(perm))
+                    c1 = perm[r]
+                    i= 0
+        print("p2", weaklyMatrix)
+        return weaklyMatrix,weaklyP2_action
